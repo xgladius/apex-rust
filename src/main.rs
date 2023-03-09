fee7016b59e9c3e96509ad6f7bafc3023ab75fdb
xgladius <stormwizard1001@gmail.com>
2023-03-08 19:36:59 -0600

    antirecoil + fix localplayer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use libm::fmodf;
 use pelite::pe64::{Pe, PeFile};
 use process_memory::{
     copy_address, Architecture, DataMember, Memory, ProcessHandle, ProcessHandleExt,
     TryIntoProcessHandle,
 };
-use std::str;
 use sysinfo::{PidExt, Process, ProcessExt, System, SystemExt};
 
 use skidscan::signature;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Vec2 {
+    pub pitch: f32,
+    pub yaw: f32,
+}
 
 fn read<T: Copy>(handle: ProcessHandle, address: usize) -> std::io::Result<T> {
     unsafe { DataMember::<T>::new_offset(handle, vec![address]).read() }
@@ -58,7 +64,7 @@ fn main() -> std::io::Result<()> {
     let text_sec = pe_file
         .section_headers()
         .iter()
-        .find(|sec| str::from_utf8(sec.name_bytes()).unwrap() == String::from(".text"))
+        .find(|sec| std::str::from_utf8(sec.name_bytes()).unwrap() == String::from(".text"))
         .unwrap();
 
     println!("Found text section: {:#?}", text_sec);
@@ -71,36 +77,79 @@ fn main() -> std::io::Result<()> {
         loaded_text_sec.len()
     );
 
-    let local_player = read::<usize>(handle, text_base + 0x01ee8cb0 + 0x8).unwrap();
+    let local_player = read::<usize>(handle, base + 0x1EE8D58).unwrap();
     let local_team = read::<u32>(handle, local_player + 0x044c).unwrap();
+    println!(
+        "Found localplayer: 0x{:X} Team: {}",
+        local_player, local_team
+    );
 
     let client_entity_list_sig = signature!("4C 8B 15 ? ? ? ? 33 F6");
     let client_entity_list_adr = client_entity_list_sig
         .scan(loaded_text_sec.as_slice())
         .unwrap_or(0);
 
+    let i_input_system_sig = signature!("48 8B 05 ? ? ? ? 48 8D 4C 24 20 BA 01 00 00 00 C7");
+    let i_input_system_adr = i_input_system_sig
+        .scan(loaded_text_sec.as_slice())
+        .unwrap_or(0);
+    if i_input_system_adr > 0 {
+        println!("Found IInputSystem: 0x{:X}", i_input_system_adr);
+    }
+
+    let sensitivty = read::<f32>(handle, base + 0x01ed55b0)?;
+    println!("Got sensitivty: {}", sensitivty);
+
+    let mut last_pitch_yaw = Vec2 {
+        pitch: 0.0f32,
+        yaw: 0.0f32,
+    };
+
     while client_entity_list_adr > 0 {
         let client_entity_list =
             resolve_relative(handle, client_entity_list_adr + text_base, 3, 7)? + 0x8;
-        //println!("Found IClientEntityList: 0x{:X}", client_entity_list);
+
+        if client_entity_list <= 0 {
+            continue;
+        }
+
+        let weapon_pitch_yaw = read::<Vec2>(handle, local_player + 0x24B0)?;
+        let mut view_angles_pitch_yaw = read::<Vec2>(handle, local_player + 0x25ac - 0x14)?;
+
+        if weapon_pitch_yaw.pitch != 0.0f32 {
+            let delta = weapon_pitch_yaw.pitch - last_pitch_yaw.pitch;
+            if delta <= 90.0f32 || delta >= -90.0f32 {
+                view_angles_pitch_yaw.pitch = view_angles_pitch_yaw.pitch - delta;
+            }
+            last_pitch_yaw.pitch = weapon_pitch_yaw.pitch;
+        }
+
+        if weapon_pitch_yaw.yaw != 0.0f32 {
+            let delta = weapon_pitch_yaw.yaw - last_pitch_yaw.yaw;
+            if delta <= 90.0f32 || delta >= -90.0f32 {
+                view_angles_pitch_yaw.yaw = view_angles_pitch_yaw.yaw - delta;
+            }
+            last_pitch_yaw.yaw = weapon_pitch_yaw.yaw;
+        }
+
+        write::<Vec2>(handle, local_player + 0x25ac - 0x14, view_angles_pitch_yaw)?;
 
         for i in 0..70 {
             let entity = get_entity(handle, client_entity_list, i)?;
             if entity > 0 {
                 let health = read::<u32>(handle, entity + 0x043C)?;
                 let team = read::<u32>(handle, entity + 0x044c)?;
+                let life_state = read::<u32>(handle, entity + 0x0798)?;
 
-                //println!("Entity: 0x{:X} Health: {} Team: {}", entity, health, team);
-
-                if team != local_team {
-                    write::<i32>(handle, entity + 0x2C4, 1512990053)?;
-                    write::<i32>(handle, entity + 0x3C8, 1)?;
-                    write::<i32>(handle, entity + 0x3D0, 1)?;
-                    write::<f32>(handle, entity + 0x3B4, 99999999.0)?;
-                    write::<f32>(handle, entity + 0x1D0, 255.0)?;
-                } else {
-                    write::<f32>(handle, entity + 0x1D4, 255.0)?;
+                if team == local_team || health == 0 || life_state != 0 || entity == local_player {
+                    continue;
                 }
+
+                write::<i32>(handle, entity + 0x2C4, 1512990053)?;
+                write::<i32>(handle, entity + 0x3C8, 1)?;
+                write::<i32>(handle, entity + 0x3D0, 1)?;
+                write::<f32>(handle, entity + 0x3B4, 99999999.0)?;
+                write::<f32>(handle, entity + 0x1D0, 255.0)?;
             }
         }
     }
